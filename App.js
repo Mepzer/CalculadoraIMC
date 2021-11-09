d8a34e39839fa81c984f256b2954909d54296934
Mepzer <wesleyyads@gmail.com>
2021-11-09 08:35:30 -0300

    Primeira versão

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function executarCalculos(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    alert("Seu IMC é: " + imc.toFixed(2));
+  }
+
+  return(
+    <View style={estilo.container} backgroundColor="#2c2f33">
+      <Text style={estilo.title}>Calcule seu IMC
+      </Text>
+      <TextInput 
+        style={estilo.input}
+        value={peso}  // valor de dentro do componente
+        onChangeText={(peso)=>setPeso(peso)} //toda vez que o campo mudar a função é chamada
+        placeholder="Peso (kg)" 
+        keyboardType="numeric"     
+        backgroundColor="#Ffffff"
+      />
+      <TextInput 
+        style={estilo.input}
+        value={altura}  // valor de dentro do componente
+        onChangeText={(altura)=>setAltura(altura)} //toda vez que o campo mudar a função é chamada
+        placeholder="Altura (cm)" 
+        keyboardType="numeric" 
+        backgroundColor="#ffffff"    
+      />
+      <TouchableOpacity style={estilo.botao} onPress={executarCalculos}>
+        <Text style={estilo.textoBotao}>Calcular</Text>
+      </TouchableOpacity>
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilo = StyleSheet.create({
+  container:{
+    flex:1
   },
+  title:{
+    textAlign:'center',
+    marginTop:25,
+    fontSize:30,
+    color:"#7289da"
+  },
+  input:{
+    backgroundColor:'#DDD',
+    borderRadius:10,
+    margin:15,
+    padding:10,
+    color:'#000',
+    fontSize:23
+  },
+  botao:{
+    justifyContent:'center',
+    alignItems:'center',
+    margin:15,
+    backgroundColor:'#7289da',
+    padding:10,
+  },
+  textoBotao:{
+    color:'#fff',
+    fontSize:25,
+  }
 });
